fdfe214005c291b1bd96c2d0c6dda44f2330df9a
April <sennerac@yahoo.com>
2015-02-23 15:43:04 -0800

    Added TrashFact2 page

--- app/src/main/java/com/cookie_computing/wastenomore/TrashFact.java
+++ app/src/main/java/com/cookie_computing/wastenomore/TrashFact.java
@@ -9,21 +9,90 @@ import android.view.View;
 import android.widget.TextView;
 
 
-public class TrashFact extends ActionBarActivity {
+public class TrashFact extends ActionBarActivity { //extends FragmentActivity {
+//        // When requested, this adapter returns a TrashFactFragment,
+//        // representing an object in the collection.
+//        FactPagerAdapter trashFactPagerAdapter;
+//        ViewPager viewPager;
+//
+//        public void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_trash_fact);
+//
+//            // ViewPager and its adapters use support library
+//            // fragments, so use getSupportFragmentManager.
+//            trashFactPagerAdapter =
+//                    new FactPagerAdapter(
+//                            getSupportFragmentManager());
+//            viewPager = (ViewPager) findViewById(R.id.pager);
+//            viewPager.setAdapter(trashFactPagerAdapter);
+//        }
+//    }
+//
+//    // Since this is an object collection, use a FragmentStatePagerAdapter,
+//    // and NOT a FragmentPagerAdapter.
+//    public class FactPagerAdapter extends FragmentPagerAdapter {
+//        public FactPagerAdapter(FragmentManager fm) {
+//            super(fm);
+//        }
+//
+//        @Override
+//        public Fragment getItem(int i) {
+//            Fragment fragment = new FactObjectFragment();
+//            Bundle args = new Bundle();
+//            // Our object is just an integer :-P
+//            args.putInt(FactObjectFragment.ARG_OBJECT, i + 1);
+//            fragment.setArguments(args);
+//            return fragment;
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return 100;
+//        }
+//
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//            return "OBJECT " + (position + 1);
+//        }
+//    }
+//
+//    // Instances of this class are fragments representing a single
+//    // object in our collection.
+//    public static class FactObjectFragment extends Fragment {
+//        public static final String ARG_OBJECT = "object";
+//
+//        @Override
+//        public View onCreateView(LayoutInflater inflater,
+//                                 ViewGroup container, Bundle savedInstanceState) {
+//            // The last two arguments ensure LayoutParams are inflated
+//            // properly.
+//            View rootView = inflater.inflate(
+//                    R.layout.fragment_collection_object, container, false);
+//            Bundle args = getArguments();
+//            ((TextView) rootView.findViewById(android.R.id.text1)).setText(
+//                    Integer.toString(args.getInt(ARG_OBJECT)));
+//            return rootView;
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trash_fact);
 
-        // Get the message from the intent
+        // Get the intent
         Intent intent = getIntent();
 
-        String message = "The average person generates over 4 pounds of trash every day and " +
+        String fact = "The average person generates over 4 pounds of trash every day and " +
                 "about 1.5 tons of solid waste per year.";
-        // Create the text view
+        // Get the text view and set text
         final TextView textView = (TextView) findViewById(R.id.trashFact);
-        textView.setText(message);
+        textView.setText(fact);
+
+        String source = getResources().getString(R.string.trash_fact_1_source);
+        final TextView textSourceView = (TextView) findViewById(R.id.trashFactSource);
+        textSourceView.setText(source);
 
     }
 
@@ -53,6 +122,12 @@ public class TrashFact extends ActionBarActivity {
     /** Called when the user clicks the Home button */
     public void goHome(View view) {
         Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the Next Fact button */
+    public void nextFact(View view) {
+        Intent intent = new Intent(this, TrashFact2.class);
         startActivity(intent);
     }
 }
